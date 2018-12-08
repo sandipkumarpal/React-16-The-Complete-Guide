1a1d21f65ddbda3afc76aa9beccf043fcaac4ee2
Sandip Pal <sandip.pal18@gmail.com>
2018-12-08 18:05:33 +0530

    Adds ToolBar Component and Css

--- basic/src/components/Layout/index.js
+++ basic/src/components/Layout/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import HoCAux from '../../hoc/HoCAux';
 
 import '../../stylesheets/components/Layout/layout.css';
+import ToolBar from '../ToolBar';
 
 function Layout(props) {
     return (
         <HoCAux>
-            <header>
-                ToolBar, SideDrawer, BacakDrop
-            </header>
+            <ToolBar />
             <main className="content">
                 {props.children}
             </main>
---
+++ basic/src/components/ToolBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../stylesheets/components/ToolBar.css';
+
+function ToolBar(props) {
+    return (
+      <header className="ToolBar">
+        <div>Menu</div>
+        <div>Logo</div>
+        <nav>
+          ...
+        </nav>
+      </header>
+    );
+}
+
+export default ToolBar;
